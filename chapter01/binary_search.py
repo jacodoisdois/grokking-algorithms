3bd496ead504cc11c88729d4fceefc653d3e554a
Jacó Magalhães <jacomagalhaes@gmail.com>
2021-12-07 01:47:44 -0300

    Binary search example

---
+++ chapter01/binary_search.py
@@ -0,0 +1,18 @@
+list = [1,2,3,4,5,6,7,8,9,10]
+
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high)
+        guess = list[mid]
+        if guess == item:
+            return mid 
+        elif guess > item:
+            high = mid -1
+        else:
+            low = mid + 1
+    return None
+
+print(binary_search(list,6))
